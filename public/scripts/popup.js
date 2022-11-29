883bf2a6ec99f9e5bc66b0009b5faf91bb8c7f23
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-11-29 10:36:47 +0100

    regler popup

---
+++ public/scripts/popup.js
@@ -0,0 +1,13 @@
+function showRulesPopup() {
+
+ var popup = document.getElementById("popup");
+ popup.classList.add("open-popup");
+
+};
+
+function closeRulesPopup() {
+
+ var popup = document.getElementById("popup");
+ popup.classList.remove("open-popup");
+
+}
