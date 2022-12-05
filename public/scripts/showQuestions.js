49033b37ac605c598361b44781d351173f65f79e
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-12-05 16:40:22 +0100

    Først buzze ind når countdown er 0

--- public/scripts/showQuestions.js
+++ public/scripts/showQuestions.js
@@ -1,11 +1,10 @@
 
 const RESPONCE_TIME_IN_MILLISECONDS = 1000;
 
-
 let clue; 
 
 // row 1
-let cells =  document.querySelectorAll('.first th')      
+let cells =  document.querySelectorAll('.first th');    
 for (var i = 0; i < cells.length; i++) {
  cells[i].onclick = function () {  clickRow1( this.getAttribute("value")); };
 };
@@ -15,7 +14,7 @@ async function clickRow1(i) {
 };
 
 // row 2
-let cells2 = document.querySelectorAll('.second th')
+let cells2 = document.querySelectorAll('.second th');
 for (var i = 0; i < cells2.length; i++) {
  cells2[i].onclick = function () { clickRow2(this.getAttribute("value")); };
 };
@@ -25,7 +24,7 @@ async function clickRow2(i) {
 };
 
 // row 3
-let cells3 = document.querySelectorAll('.third th')
+let cells3 = document.querySelectorAll('.third th');
 for (var i = 0; i < cells3.length; i++) {
  cells3[i].onclick = function () { clickRow3(this.getAttribute("value")); };
 };
@@ -34,8 +33,8 @@ async function clickRow3(i) {
  showQuestionPopup();
 };
 
-// cell row 4
-let cells4 = document.querySelectorAll('.forth th')
+// row 4
+let cells4 = document.querySelectorAll('.forth th');
 for (var i = 0; i < cells4.length; i++) {
  cells4[i].onclick = function () { clickRow4(this.getAttribute("value")); };
 };
@@ -44,8 +43,8 @@ async function clickRow4(i) {
  showQuestionPopup();
 };
 
-// cell row 5
-let cells5 = document.querySelectorAll('.fifth th')
+// row 5
+let cells5 = document.querySelectorAll('.fifth th');
 for (var i = 0; i < cells5.length; i++) {
  cells5[i].onclick = function () { clickRow5(this.getAttribute("value")); };
 };
@@ -59,12 +58,12 @@ async function loadQuestion(value, categori) {
 
     let categorieIDs = [];
 
-    categorieIDs.push(sessionStorage.getItem("cat1"))
-    categorieIDs.push(sessionStorage.getItem("cat2"))
-    categorieIDs.push(sessionStorage.getItem("cat3"))
-    categorieIDs.push(sessionStorage.getItem("cat4"))
-    categorieIDs.push(sessionStorage.getItem("cat5"))
-    categorieIDs.push(sessionStorage.getItem("cat6"))
+    categorieIDs.push(sessionStorage.getItem("cat1"));
+    categorieIDs.push(sessionStorage.getItem("cat2"));
+    categorieIDs.push(sessionStorage.getItem("cat3"));
+    categorieIDs.push(sessionStorage.getItem("cat4"));
+    categorieIDs.push(sessionStorage.getItem("cat5"));
+    categorieIDs.push(sessionStorage.getItem("cat6"));
 
     console.log(categori);
 
@@ -80,28 +79,76 @@ async function loadQuestion(value, categori) {
 
     return data[value];
 
-}
+};
 
 console.log(document.querySelectorAll('.first th'));
 
 
 // ------------------------------ QUESTION POPUP ------------------------------------
 
-
 let startTimer;
 
-
 function showQuestionPopup() {
-// ------------------------------ BUZZERS -------------------------------------------
+
+ var popup = document.getElementById("Question_popup");
+ popup.classList.add("question-open-popup");
+
+ var popup = document.getElementById("fadeQuestion_popup_background");
+ popup.classList.add("fade");
+
+ document.getElementById("question_popup_H2").innerHTML += " til " + clue.value;
+
+ document.getElementById("question_popup_H2").innerHTML += "<h3>" + clue.question + "</h3>";
+
+ startTimer = window.setInterval(inputTimer, RESPONCE_TIME_IN_MILLISECONDS);
+
+};
+
+function flipCardDelay(){
+ document.getElementById("thecard").classList.add("flipcard");
+ startTimer = window.setInterval(inputTimer, RESPONCE_TIME_IN_MILLISECONDS);
+};
+
+function closeQuestionPopup() {
+ var popup = document.getElementById("Question_popup");
+ popup.classList.remove("question-open-popup");
+
+ var popup = document.getElementById("fadeQuestion_popup_background");
+ popup.classList.remove("fade");
+};
+// --------------------------------------------------------------------------------
+
+// ------------------------------ TIMER -------------------------------------------
+let timer = 100;
+
+function inputTimer(){
+
+ let numberTimeout = document.getElementById("numberTimeout");
+
+ numberTimeout.innerHTML = timer/10 * RESPONCE_TIME_IN_MILLISECONDS/1000 + " sekunder..."
+
+ if (timer == 0) {
+  clearTimeout(startTimer);
+  console.log("tiden er udløbet og der må svares...");
+  activateBuzzers();
+  document.getElementById("numberTimeout").innerHTML = "DER MÅ NU BUZZES !!!";
+ };
+
+ timer = timer - 10;
+
+};
+
+//------------------------------ ACTIVATE BUZZERS & SOUND ------------------------------------------------
 let firstToBuzz = "";
-document.getElementById("firstToBuzzH2").innerHTML = "";
-
-let key1 = sessionStorage.getItem("buzzer1Label");
-let key2 = sessionStorage.getItem("buzzer2Label");
-let key3 = sessionStorage.getItem("buzzer3Label");
-let key4 = sessionStorage.getItem("buzzer4Label");
-
+function activateBuzzers(){
  document.onkeydown = function (e) {
+
+  document.getElementById("firstToBuzzH2").innerHTML = "";
+  
+  let key1 = sessionStorage.getItem("buzzer1Label");
+  let key2 = sessionStorage.getItem("buzzer2Label");
+  let key3 = sessionStorage.getItem("buzzer3Label");
+  let key4 = sessionStorage.getItem("buzzer4Label");
 
   console.log(e.key);
 
@@ -140,100 +187,29 @@ let key4 = sessionStorage.getItem("buzzer4Label");
    document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player4") + " var først!";
    window.setTimeout(flipCardDelay, 1000);
   };
+ };
 };
-
- var popup = document.getElementById("Question_popup");
- popup.classList.add("question-open-popup");
-
- var popup = document.getElementById("fadeQuestion_popup_background");
- popup.classList.add("fade");
-
- document.getElementById("question_popup_H2").innerHTML += " til " + clue.value;
-
- document.getElementById("question_popup_H2").innerHTML += "<h3>" + clue.question + "</h3>";
-
-
- startTimer = window.setInterval(inputTimer, RESPONCE_TIME_IN_MILLISECONDS);
-
-
-
-
-
-
-};
-
-function flipCardDelay(){
- document.getElementById("thecard").classList.add("flipcard");
-
- startTimer = window.setInterval(inputTimer, RESPONCE_TIME_IN_MILLISECONDS);
-
-}
-
-function closeQuestionPopup() {
-
- var popup = document.getElementById("Question_popup");
- popup.classList.remove("question-open-popup");
-
- var popup = document.getElementById("fadeQuestion_popup_background");
- popup.classList.remove("fade");
-
-
-};
-// --------------------------------------------------------------------------------
 
 function playBuzzer() {
-  var audio = new Audio("../sounds/buzz2.wav");
-  audio.play();
+ var audio = new Audio("../sounds/buzz2.wav");
+ audio.play();
 };
 
-// ------------------------------ TIMER -------------------------------------------
-let timer = 100;
+//--------------------------------------------------------------------------------------------------------
 
-function inputTimer(){
-
- // let answerBlock = document.getElementById("questionBlock");
-
- // console.log(timer);
-
- // let test = "questionBlock" + timer;
- // console.log(test);
-
- // answerBlock.classList.remove(test);
- 
- let numberTimeout = document.getElementById("numberTimeout");
-
- numberTimeout.innerHTML = timer/10 * RESPONCE_TIME_IN_MILLISECONDS/1000 + " sekunder..."
-
- if (timer == 0) {
-  clearTimeout(startTimer);
- };
-
- timer = timer - 10;
-
- // answerBlock.classList.add(test);
-
-};
-
+//------------SVAR KNAP CLICK OG VALIDERING---------------------------------------------------------------
 document.getElementById("answerButton").onclick = function () { 
 
-    let answerText = document.getElementById("answerInput").value;
+  let answerText = document.getElementById("answerInput").value;
 
-    console.log(answerText);
-    console.log(clue.answer);
+  console.log(answerText);
+  console.log(clue.answer);
+  console.log(checkAnswer(answerText));
 
-    console.log(checkAnswer(answerText));
-
-
- };
-
-
-
+};
 
 function checkAnswer(answer) {
-    return (answer.toUpperCase() == clue.answer.toUpperCase() );
+    return (answer.toUpperCase() == clue.answer.toUpperCase());
+};
 
-}
-
-
-
-// --------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------
