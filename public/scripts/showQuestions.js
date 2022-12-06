32e8430f559858a56a2064aad5b59c7f567764c3
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-12-06 17:09:19 +0100

    Refactoring af loadCategories

--- public/scripts/showQuestions.js
+++ public/scripts/showQuestions.js
@@ -65,9 +65,9 @@ async function loadQuestion(value, categori) {
     categorieIDs.push(sessionStorage.getItem("cat5"));
     categorieIDs.push(sessionStorage.getItem("cat6"));
 
-    console.log(categori);
+    console.log("categori", categori);
 
-    console.log(categorieIDs);
+    console.log("categorieIDs", categorieIDs);
 
     let apiPath = `https://jservice.io/api/clues?category=${categorieIDs[categori - 1]}`;
 
