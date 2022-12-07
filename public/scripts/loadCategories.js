b721e16d524b0587c0c33711e64d876d70b033a5
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-12-07 07:49:02 +0100

    renaming a vars i loadCategories + oprydning

--- public/scripts/loadCategories.js
+++ public/scripts/loadCategories.js
@@ -4,49 +4,48 @@ function shuffle(array) {
  var m = array.length, t, i;
  // While there remain elements to shuffle…
  while (m) {
-   // Pick a remaining element…
-   i = Math.floor(Math.random() * m--);
-   // And swap it with the current element.
-   t = array[m];
-   array[m] = array[i];
-   array[i] = t;
+  // Pick a remaining element…
+  i = Math.floor(Math.random() * m--);
+  // And swap it with the current element.
+  t = array[m];
+  array[m] = array[i];
+  array[i] = t;
  };
  return array;
 };
 // Hent data fra API via JSON
 async function loadCategories() {
  try {
-  let randomCatOffset = Math.round(Math.random()*8000);
-  let apiPath = `https://jservice.io/api/categories?count=100&offset=${randomCatOffset}`;
+  let randomCategoryOffset = Math.round(Math.random()*8000);
+  let apiPath = `https://jservice.io/api/categories?count=100&offset=${randomCategoryOffset}`;
   const response = await fetch(apiPath);
   const data = await response.json();
   console.log("Raw data, categories:", data);
-  // Test for tomme kategorier og byg cats med 60 kategorier:
-  // rename "cats"
-  let cats = [];
-  for (i = 0; cats.length < 60; i++){
-   //4068 er "name that artist" og burde vist indeholde billeder...
+  // Test for tomme kategorier og byg categories med 60 kategorier:
+  let categories = [];
+  for (i = 0; categories.length < 60; i++){
+   //4068 er "name that artist" og burde vist indeholde billeder. Så den er sorteret fra her.
     if (data[i].clues_count > 4 && data.id != 4068){
-     cats.push(data[i]);
+     categories.push(data[i]);
     };
   };
-  //Randomize cats array:
-  shuffle(cats);
+  //Randomize categories array:
+  shuffle(categories);
    // Load dropdowns:
-   for (let i = 0; i < 12; i++) {
-    let choice = document.getElementById(`kategori${i+1}`);
-    choice.innerHTML = "";
-    if (showCategoriHeading) {
-     choice.innerHTML += `<option value='default'>Vælg kategori</option>`;
-    }
-    for (j = 0; j < 5; j++){
-     let counter = (j)+i*5;
-     choice.innerHTML += `<option value=${cats[counter].id}>${cats[counter].title}</option>`;
-     console.log(counter);
-     console.log()
-    };
+  for (let i = 0; i < 12; i++) {
+   let choice = document.getElementById(`kategori${i+1}`);
+   choice.innerHTML = "";
+   if (showCategoriHeading) {
+    choice.innerHTML += `<option value='default'>Vælg kategori</option>`;
+   }
+   for (j = 0; j < 5; j++){
+    let counter = (j)+i*5;
+    choice.innerHTML += `<option value=${categories[counter].id}>${categories[counter].title}</option>`;
+    console.log(counter);
+    console.log();
    };
-   showCategoriHeading = false;
+  };
+  showCategoriHeading = false;
  } catch (e) {
   console.log(e);
   console.log("There was an error fetching the data");
