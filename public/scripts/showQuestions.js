59f3a3e8b0d8003127e58c1502cece522c6fe3fc
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-12-13 09:27:55 +0100

    Timer etc.

--- public/scripts/showQuestions.js
+++ public/scripts/showQuestions.js
@@ -1,11 +1,21 @@
-
-const RESPONCE_TIME_IN_MILLISECONDS = 1000;
+let timer = 23;
+let answerTimerInSeconds = 30;
 let isAnswerCorrect = false;
 let round = 1;
 let currentPoints = 0; 
-let timer = 100;
-let clue; 
+let clue;
 
+// // set css timeCountdown:
+var root = document.querySelector(':root');
+let seconds = timer + 's';
+var timeCountdown = root.style.setProperty('--timeCountdown', seconds);
+// get css timeCountdown:
+var rootStyles = getComputedStyle(root);
+var timeCountdown= rootStyles.getPropertyValue('--timeCountdown');
+console.log("timeCountdown", timeCountdown);
+
+
+// rename 'first', 'second', etc. og refac.!:
 // row 1
 let cells =  document.querySelectorAll('.first th');    
 for (var i = 0; i < cells.length; i++) {
@@ -19,7 +29,6 @@ async function clickRow1(i) {
 // row 2
 let cells2 = document.querySelectorAll('.second th');
 for (var i = 0; i < cells2.length; i++) {
-
  cells2[i].onclick = function () { clickRow2(this.getAttribute("value")); };
 };
 async function clickRow2(i) {
@@ -57,13 +66,11 @@ async function clickRow5(i) {
  showQuestionPopup();
 };
 
-
 async function loadQuestion(value, categori) {
 
     currentPoints = (value + 1) * (round * 100);
 
     let categorieIDs = [];
-
     categorieIDs.push(sessionStorage.getItem("cat1"));
     categorieIDs.push(sessionStorage.getItem("cat2"));
     categorieIDs.push(sessionStorage.getItem("cat3"));
@@ -71,185 +78,155 @@ async function loadQuestion(value, categori) {
     categorieIDs.push(sessionStorage.getItem("cat5"));
     categorieIDs.push(sessionStorage.getItem("cat6"));
 
-    console.log("categori", categori);
-
-    console.log("categorieIDs", categorieIDs);
-
     sessionStorage.setItem("activeCategory", categori - 1);
     sessionStorage.setItem("activeQuestion", (value + 1));
 
     let apiPath = `https://jservice.io/api/clues?category=${categorieIDs[categori - 1]}`;
 
     const response = await fetch(apiPath);
-
     const data = await response.json();
-
-    console.log(data[value]);
-
+    // console.log(data[value]);
     return data[value];
-
 };
-
-console.log(document.querySelectorAll('.first th'));
-
+// console.log(document.querySelectorAll('.first th'));
 
 // ------------------------------ QUESTION POPUP ------------------------------------
-
-let startTimer;
-
+let startQuestionTimer;
 function showQuestionPopup() {
 
-  timer = 100;
+ document.getElementById("backQuestion").classList.add("hide");
+ document.getElementById("question_popup_H2").innerHTML = "Spørgsmål";
+ document.getElementById("thecard").classList.remove("flipcard");
 
-  document.getElementById("backQuestion").classList.add("hide");
+ var popup = document.getElementById("Question_popup");
+ popup.classList.add("question-open-popup");
 
-  document.getElementById("question_popup_H2").innerHTML = "Spørgsmål";
-
-  document.getElementById("thecard").classList.remove("flipcard");
-
-  var popup = document.getElementById("Question_popup");
-  popup.classList.add("question-open-popup");
-
-  var popup = document.getElementById("fadeQuestion_popup_background");
-  popup.classList.add("fade");
+ var popup = document.getElementById("fadeQuestion_popup_background");
+ popup.classList.add("fade");
 
  document.getElementById("question_popup_H2").innerHTML += " til " + currentPoints;
-
  document.getElementById("question_popup_H2").innerHTML += "<h3>" + clue.question + "</h3>";
 
- startTimer = window.setInterval(inputTimer, RESPONCE_TIME_IN_MILLISECONDS);
-
+ startQuestionTimer = window.setInterval(inputTimer, 1000);
 };
 
+let startAnswerTimer;
 function flipCardDelay(){
-
-
-  document.getElementById("backQuestion").classList.remove("hide");
-
+ document.getElementById("backQuestion").classList.remove("hide");
  document.getElementById("thecard").classList.add("flipcard");
- startTimer = window.setInterval(inputTimer, RESPONCE_TIME_IN_MILLISECONDS);
+ // bagside=?:
+ // startQuestionTimer = window.setInterval(inputTimer, RESPONCE_TIME_IN_MILLISECONDS);
+ document.getElementById("frontQuestion").classList.add("hide")
+ document.getElementById("question_popup_H2_Back").innerHTML = "Spørgsmål"
+ document.getElementById("answerButton").classList.remove("hide");
+ document.getElementById("answerInput").readOnly = false;
+ document.getElementById("answerInput").value = "";
+ answerResponce.innerText = "";
 
-  document.getElementById("frontQuestion").classList.add("hide")
+ startAnswerTimer = window.setInterval(answerTimer, 1000);
 
-  document.getElementById("question_popup_H2_Back").innerHTML = "Spørgsmål"
-
-  document.getElementById("answerButton").classList.remove("hide");
-  document.getElementById("answerInput").readOnly = false;
-  document.getElementById("answerInput").value = "";
-  answerResponce.innerText = "";
-  document.getElementById("continueButton").classList.add("hide");
-  document.getElementById("itWasRightButton").classList.add("hide");
-
-
-
-
-
-  document.getElementById("question_popup_H2_Back").innerHTML += " til " + currentPoints;
-
-  document.getElementById("question_popup_H2_Back").innerHTML += "<h3>" + clue.question + "</h3>";
-
+ document.getElementById("continueButton").classList.add("hide");
+ document.getElementById("itWasRightButton").classList.add("hide");
+ document.getElementById("question_popup_H2_Back").innerHTML += " til " + currentPoints;
+ document.getElementById("question_popup_H2_Back").innerHTML += "<h3>" + clue.question + "</h3>";
+ 
 };
 
 function closeQuestionPopup() {
  var popup = document.getElementById("Question_popup");
  popup.classList.remove("question-open-popup");
-
  var popup = document.getElementById("fadeQuestion_popup_background");
  popup.classList.remove("fade");
 
-  document.getElementById("frontQuestion").classList.remove("hide")
-
+ document.getElementById("frontQuestion").classList.remove("hide")
 };
 // --------------------------------------------------------------------------------
 
 // ------------------------------ TIMER -------------------------------------------
-
-
 function inputTimer(){
-
- //TODO:
- //Lav css countdown animation + div:
-
-
 
  let numberTimeout = document.getElementById("numberTimeout");
  let textTimeout = document.getElementById("textTimeout");
-
  numberTimeout.classList.add("numberTimeout");
  textTimeout.classList.add("textTimeout");
-
- timeleft = RESPONCE_TIME_IN_MILLISECONDS;
-
- numberTimeout.innerHTML = (timer/10 * timeleft/1000) + " sekunder...";
-
+ // timeleft bruges ikke?:
+ // timeleft = RESPONCE_TIME_IN_MILLISECONDS;
+ numberTimeout.innerHTML = timer + " sekunder...";
  if (timer <= 0) {
-  clearTimeout(startTimer);
+  clearTimeout(startQuestionTimer);
   console.log("tiden er udløbet og der må svares...");
   activateBuzzers();
-
   numberTimeout.innerHTML = "DER MÅ NU BUZZES !!!";
  };
-
- timer = timer - 10;
-
+ timer = timer - 1;
 };
 
+
+function answerTimer(){
+document.getElementById("countdownAnswer").innerHTML = answerTimerInSeconds;
+ if (answerTimerInSeconds <= 0) {
+  clearTimeout(startAnswerTimer);
+  // TODO: Giv besked og luk question popup:
+  window.alert("Tiden løb ud");
+ };
+ answerTimerInSeconds = answerTimerInSeconds - 1;
+};
 //------------------------------ ACTIVATE BUZZERS & SOUND ------------------------------------------------
 let firstToBuzz = "";
 function activateBuzzers(){
+
  document.onkeydown = function (e) {
+ document.getElementById("firstToBuzzH2").innerHTML = "";
+ 
+ let key1 = sessionStorage.getItem("buzzer1Label");
+ let key2 = sessionStorage.getItem("buzzer2Label");
+ let key3 = sessionStorage.getItem("buzzer3Label");
+ let key4 = sessionStorage.getItem("buzzer4Label");
 
-  document.getElementById("firstToBuzzH2").innerHTML = "";
+ console.log(e.key);
+
+ console.log(key1);
+
+ if (e.key !== key1 && e.key !== key2 && e.key !== key3 && e.key !== key4) {
+   return;
+ };
+
+ if (firstToBuzz == "" && e.key == key1) {
+  firstToBuzz = e.key;
+  console.log(sessionStorage.getItem("player1"), "var først!");
+  playBuzzer();
+  document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player1") + " var først!";
+  window.setTimeout(flipCardDelay, 1000);
+  sessionStorage.setItem("playerAnswering", sessionStorage.getItem("player1"));
   
-  let key1 = sessionStorage.getItem("buzzer1Label");
-  let key2 = sessionStorage.getItem("buzzer2Label");
-  let key3 = sessionStorage.getItem("buzzer3Label");
-  let key4 = sessionStorage.getItem("buzzer4Label");
-
-  console.log(e.key);
-
-   console.log(key1);
-
-  if (e.key !== key1 && e.key !== key2 && e.key !== key3 && e.key !== key4) {
-    return;
-  };
-
-  if (firstToBuzz == "" && e.key == key1) {
-   firstToBuzz = e.key;
-   console.log(sessionStorage.getItem("player1"), "var først!");
-   playBuzzer();
-   document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player1") + " var først!";
-   window.setTimeout(flipCardDelay, 1000);
-   sessionStorage.setItem("playerAnswering", sessionStorage.getItem("player1"));
-   
-  };
+ };
   
-  if (firstToBuzz == "" && e.key == key2) {
-   firstToBuzz = e.key;
-   console.log(sessionStorage.getItem("player2"), "var først!");
-   playBuzzer();
-   document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player2") + " var først!";
-   window.setTimeout(flipCardDelay, 1000);
-   sessionStorage.setItem("playerAnswering", sessionStorage.getItem("player2"));
-  };
+ if (firstToBuzz == "" && e.key == key2) {
+  firstToBuzz = e.key;
+  console.log(sessionStorage.getItem("player2"), "var først!");
+  playBuzzer();
+  document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player2") + " var først!";
+  window.setTimeout(flipCardDelay, 1000);
+  sessionStorage.setItem("playerAnswering", sessionStorage.getItem("player2"));
+ };
 
-  if (firstToBuzz == "" && e.key == key3) {
-   firstToBuzz = e.key;
-   console.log(sessionStorage.getItem("player3"), "var først!");
-   playBuzzer();
-   document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player3") + " var først!";
-   window.setTimeout(flipCardDelay, 1000);
-   sessionStorage.setItem("playerAnswering", sessionStorage.getItem("player3"));
-  };
+ if (firstToBuzz == "" && e.key == key3) {
+  firstToBuzz = e.key;
+  console.log(sessionStorage.getItem("player3"), "var først!");
+  playBuzzer();
+  document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player3") + " var først!";
+  window.setTimeout(flipCardDelay, 1000);
+  sessionStorage.setItem("playerAnswering", sessionStorage.getItem("player3"));
+ };
 
-  if (firstToBuzz == "" && e.key == key4) {
-   firstToBuzz = e.key;
-   console.log(sessionStorage.getItem("player4"), "var først!");
-   playBuzzer();
-   document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player4") + " var først!";
-   window.setTimeout(flipCardDelay, 1000);
-   sessionStorage.setItem("playerAnswering", sessionStorage.getItem("player4"));
-  };
+ if (firstToBuzz == "" && e.key == key4) {
+  firstToBuzz = e.key;
+  console.log(sessionStorage.getItem("player4"), "var først!");
+  playBuzzer();
+  document.getElementById("firstToBuzzH2").innerHTML = sessionStorage.getItem("player4") + " var først!";
+  window.setTimeout(flipCardDelay, 1000);
+  sessionStorage.setItem("playerAnswering", sessionStorage.getItem("player4"));
+ };
  };
 };
 
@@ -263,84 +240,61 @@ function playBuzzer() {
 //------------SVAR KNAP CLICK OG VALIDERING---------------------------------------------------------------
 document.getElementById("answerButton").onclick = function () { 
 
+ document.getElementById("answerButton").classList.add("hide");
+ document.getElementById("answerInput").readOnly = true;
 
-  document.getElementById("answerButton").classList.add("hide");
-  document.getElementById("answerInput").readOnly = true;
+ let answerText = document.getElementById("answerInput").value;
+ let answerResponce = document.getElementById("answerResponce");
 
-  let answerText = document.getElementById("answerInput").value;
+ if (checkAnswer(answerText)) {
+  answerResponce.innerText = "Tillykke du svarede rigtigt!"; 
+  document.getElementById("continueButton").classList.remove("hide");
+  document.getElementById("continueButton").classList.add("knap");
 
- console.log(answerText);
- console.log(clue.answer);
- console.log(checkAnswer(answerText));
+  isAnswerCorrect = true;
+   
+ } else {
+  answerResponce.innerText = "Det rigtige svar er: " + clue.answer; 
+  document.getElementById("continueButton").classList.remove("hide");
+  document.getElementById("continueButton").classList.add("knap");
+  document.getElementById("itWasRightButton").classList.remove("hide");
+  document.getElementById("itWasRightButton").classList.add("knap");
 
-  let answerResponce = document.getElementById("answerResponce");
-
-
-
-  if (checkAnswer(answerText)) {
-    answerResponce.innerText = "Tillykke du svarede rigtigt!"; 
-    document.getElementById("continueButton").classList.remove("hide");
-    document.getElementById("continueButton").classList.add("knap");
-
-    isAnswerCorrect = true;
-    
-  } else {
-    answerResponce.innerText = "Det rigtige svar er: " + clue.answer; 
-    document.getElementById("continueButton").classList.remove("hide");
-    document.getElementById("continueButton").classList.add("knap");
-
-    document.getElementById("itWasRightButton").classList.remove("hide");
-    document.getElementById("itWasRightButton").classList.add("knap");
-
-    isAnswerCorrect = false;
-    
-  }
-
-  console.log(currentPoints);
-
-  window.isAnswerCorrect = isAnswerCorrect;
-  window.currentPoints = currentPoints;
-
+  isAnswerCorrect = false;
+ };
+ window.isAnswerCorrect = isAnswerCorrect;
+ window.currentPoints = currentPoints;
 };
 
 document.getElementById("continueButton").onclick = function () {
-  closePopUpAndContinueGame();
-}
+ closePopUpAndContinueGame();
+};
 
 document.getElementById("itWasRightButton").onclick = function () {
-  window.isAnswerCorrect = true;
-  closePopUpAndContinueGame();
-}
+ window.isAnswerCorrect = true;
+ closePopUpAndContinueGame();
+};
 
-  function closePopUpAndContinueGame() {
-    
-    document.getElementById("firstToBuzzH2").innerHTML = "";
-    document.getElementById("numberTimeout").innerHTML = "";
+function closePopUpAndContinueGame() {
+  
+ document.getElementById("firstToBuzzH2").innerHTML = "";
+ document.getElementById("numberTimeout").innerHTML = "";
+ document.getElementById("frontQuestion").classList.remove("hide");
 
+ firstToBuzz = "";
+ document.onkeydown = null;
 
-    document.getElementById("frontQuestion").classList.remove("hide")
-
-    firstToBuzz = "";
-    document.onkeydown = null;
-
-    adjustScore();
-
-    removeQuestion()
-
-    closeQuestionPopup();
-
-  }
+ adjustScore();
+ removeQuestion()
+ closeQuestionPopup();
+};
 
 
 function checkAnswer(answer) {
- // const answerCorrected = answer.replace(/[^a-zA-Z ]/g, "");
- // const clueCorrected = clue.answer.replace(/[^a-zA-Z ]/g, "");
  return (answer.toUpperCase() == clue.answer.toUpperCase());
 };
 
 function adjustScore() {
-  console.log("players", getPlayers());
-
   const players = getPlayers();
   const playerAnswering = sessionStorage.getItem("playerAnswering");
   let player;
@@ -348,65 +302,52 @@ function adjustScore() {
 
   if (playerAnswering) {
     player = players.find(player => player.name === playerAnswering);
-  }
+  };
 
   console.log("adjustScore: ", sessionStorage.getItem("pointsPlayer" + player.id));
-  sessionStorage.getItem("pointsPlayer" + player.id)
+  sessionStorage.getItem("pointsPlayer" + player.id);
 
   if (window.isAnswerCorrect) {
     const currentPlayerScore = parseInt(sessionStorage.getItem("pointsPlayer" + player.id));
     newPlayerScore = currentPlayerScore + window.currentPoints;
-
     sessionStorage.setItem("pointsPlayer" + player.id, newPlayerScore.toString());
-
   } else {
     const currentPlayerScore = parseInt(sessionStorage.getItem("pointsPlayer" + player.id));
     newPlayerScore = currentPlayerScore - window.currentPoints;
-
     sessionStorage.setItem("pointsPlayer" + player.id, newPlayerScore.toString());
-  }
-  
+  };
   player.score = newPlayerScore;
-
   document.querySelector(`#card${player.id} > div.points > p`).innerHTML = player.score;
-
-}
+};
 
 
 function removeQuestion() {
-  
-
+ 
   if (sessionStorage.getItem("activeQuestion") == 1) {
     sessionStorage.setItem("activeQuestion", "first");
-  } 
+  }; 
 
   if (sessionStorage.getItem("activeQuestion") == 2) {
     sessionStorage.setItem("activeQuestion", "second");
-  } 
+  }; 
 
   if (sessionStorage.getItem("activeQuestion") == 3) {
     sessionStorage.setItem("activeQuestion", "third");
-  } 
+  }; 
 
   if (sessionStorage.getItem("activeQuestion") == 4) {
     sessionStorage.setItem("activeQuestion", "fourth");
-  }  
+  };  
 
   if (sessionStorage.getItem("activeQuestion") == 5) {
     sessionStorage.setItem("activeQuestion", "fifth");
-  } 
+  };
 
-  console.log("knap skal fjernes");
-  console.log(sessionStorage.getItem("activeCategory"), sessionStorage.getItem("activeQuestion"));
-
-  // console.log(document.getElementsByClassName("first").getElementByTagName('th')[0]);
-
-  console.log(document.getElementById(sessionStorage.getItem("activeQuestion")).getElementsByTagName('th')[sessionStorage.getItem("activeCategory")]);
-
+  // console.log("knap skal fjernes");
+  // console.log(sessionStorage.getItem("activeCategory"), sessionStorage.getItem("activeQuestion"));
+  // console.log(document.getElementById(sessionStorage.getItem("activeQuestion")).getElementsByTagName('th')[sessionStorage.getItem("activeCategory")]);
   const cell = document.getElementById(sessionStorage.getItem("activeQuestion")).getElementsByTagName('th')[sessionStorage.getItem("activeCategory")];
-
   cell.classList.add("removedCell");
-  
-}
+};
 
 //--------------------------------------------------------------------------------------------------------
