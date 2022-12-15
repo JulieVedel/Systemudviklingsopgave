ab4aee8c733acce9c3dbf5328e1ff11543887a10
dgh81 <89967372+dgh81@users.noreply.github.com>
2022-12-15 10:38:40 +0100

    Id: 29 - specificering af kategoriknapper

--- public/scripts/loadCategories.js
+++ public/scripts/loadCategories.js
@@ -1,5 +1,5 @@
 // let data;
-let showCategoriHeading = true;
+// let showCategoriHeading = true;
 //Stjålet fra: https://bost.ocks.org/mike/shuffle/ og https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
  var m = array.length, t, i;
@@ -38,9 +38,9 @@ async function loadCategories() {
   for (let i = 0; i < 12; i++) {
    let choice = document.getElementById(`kategori${i+1}`);
    choice.innerHTML = "";
-   if (showCategoriHeading) {
+   // if (showCategoriHeading) {
     choice.innerHTML += `<option value='default'>Vælg kategori</option>`;
-   }
+   // }
    for (j = 0; j < 5; j++){
     let counter = (j)+i*5;
     choice.innerHTML += `<option value=${categories[counter].id}>${categories[counter].title}</option>`;
@@ -48,7 +48,7 @@ async function loadCategories() {
     console.log();
    };
   };
-  showCategoriHeading = false;
+  // showCategoriHeading = false;
  } catch (e) {
   console.log(e);
   console.log("There was an error fetching the data");
@@ -57,6 +57,67 @@ async function loadCategories() {
 
 loadCategories();
 
+async function randomCategories() {
+ console.log("running randomCategories");
+ try {
+  let randomCategoryOffset = Math.round(Math.random()*8000);
+  let apiPath = `https://jservice.io/api/categories?count=100&offset=${randomCategoryOffset}`;
+  const response = await fetch(apiPath);
+  let data = await response.json();
+  console.log("Raw data, categories:", data);
+
+  let categories = [];
+  for (i = 0; categories.length < 60; i++){
+
+    if (data[i].clues_count > 4 && data.id != 4068){
+     // denne skal ændres:
+     categories.push(data[i]);
+    };
+  };
+
+  shuffle(categories);
+
+  let remainingCategoriIDs = [];
+
+  for (let i = 0; i < 12; i++) {
+   let choice = document.getElementById(`kategori${i+1}`);
+   
+   if (choice.value == "default" || choice.value == "") {
+    console.log("choice.id", choice.id);
+    remainingCategoriIDs[i] = choice.id;
+   };
+  };
+  console.log("remainingCategoriIDs",remainingCategoriIDs);
+  
+  for (let k = 0; k < remainingCategoriIDs.length; k++) {
+   let choice = document.getElementById(`kategori${k+1}`);
+   if (choice.id == remainingCategoriIDs[k]) {
+    
+    choice.innerHTML = "";
+    choice.innerHTML += `<option value='default'>Vælg kategori</option>`;
+
+    for (j = 0; j < 5; j++){
+     if (choice.id != remainingCategoriIDs[i]) {
+     let choice = document.getElementById(`kategori${k+1}`);
+     // console.log("choice",choice);
+     // console.log("remainingCategoriIDs[i]",remainingCategoriIDs[k]);
+     choice.innerHTML += `<option value=${categories[j+k*5].id}>${categories[j+k*5].title}</option>`;
+     };
+     console.log();
+    };
+   };
+  };
+   
+
+
+
+
+  // showCategoriHeading = false;
+ } catch (e) {
+  console.log(e);
+  console.log("There was an error fetching the data");
+ };
+};
 
 // async function loadRemainingCategories() {
 //  console.log("running loadRemainingCategories");
